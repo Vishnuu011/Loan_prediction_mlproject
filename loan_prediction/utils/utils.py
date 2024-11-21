da4725a0a3fa3597b7131ccd0f98743d84202402
Vishnu <128610832+Vishnuu011@users.noreply.github.com>
2024-11-21 07:06:10 +0000

    update utils

---
+++ loan_prediction/utils/utils.py
@@ -0,0 +1,59 @@
+import yaml
+from pandas import DataFrame
+import pickle
+import numpy as np
+import sys
+import os
+
+from loan_prediction.logger import logging
+from loan_prediction.exception import CustomException
+
+
+
+def read_yml_file(file_path: str)-> dict:
+    try:
+       with open(file_path,'rb') as yaml_file:
+        return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CustomException(e,sys) from e  
+
+
+def load_object(file_path: str)-> object:
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys) from e    
+
+def save_numpy_array(file_path: str, array:np.array):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise CustomException(e,sys) from e 
+
+def load_numpy_array(file_path: str)-> np.array:
+    try:
+        with open(file_path,'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys) from e
+
+def save_object(file_path: str, obj: object)-> None:
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys) from e 
+
+def drop_columns(df: DataFrame, cols:list)-> DataFrame:
+    logging.info("Entered drop_columns methon of utils")
+    try:
+        df = df.drop(columns=cols, axis=1)
+        logging.info("Entered drop_columns methon of utils")
+        return df
+    except Exception as e:
+        raise CustomException(e,sys) from e 
