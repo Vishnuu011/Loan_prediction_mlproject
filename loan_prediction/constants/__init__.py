3968408983299522bfaa602a7d3bc4f729ade0a9
Vishnu <128610832+Vishnuu011@users.noreply.github.com>
2024-11-21 07:49:45 +0000

    update constants

---
+++ loan_prediction/constants/__init__.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+DATA_BASE_NAME="LOAN_DATA"
+COLLECTION_NAME="loan_data"
+CONNECTION_URL="MONGODB_URL"
+
+PIPELINE_NAME : str = "loanpredictionproject"
+ARTIFACT_DIR : str = "artifact"
+
+MODEL_NAME_FILE = "model.pkl"
+
+#DATAINGESTION RELATED CONSTANTS
+
+DATA_INGESTION_COLLECTION_NAME: str ="loan_data"
+DATA_INGESTION_DIR_NAME: str ="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR_NAME: str ="feature_store"
+DATA_INGESTION_INGESTED_DIR_NAME: str ="ingested"
+DATA_INGESTION_TRAIN_DIR_NAME: str ="train"
+DATA_INGESTION_TEST_DIR_NAME: str ="test"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float =0.2
